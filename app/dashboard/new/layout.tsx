963b1a1d58c3c660e0db0d441cdcff3c058ced22
automated-lab <team@weareautomated.com.au>
2025-01-09 10:48:06 +1100

    Added sharing and iframe embed 2

--- app/dashboard/new/layout.tsx
+++ app/dashboard/new/layout.tsx
@@ -3,6 +3,8 @@ import NewEventForm from "./page";
 
 export default async function Layout() {
   const session = await requireUser();
+  if (!session.user) return null;
+
   return (
     <NewEventForm username={(session.user as { username: string }).username} />
   );
