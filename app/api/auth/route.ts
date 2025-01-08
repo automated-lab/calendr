a6c32986ca7fc3643d314ecaa5df1af86d993b97
automated-lab <team@weareautomated.com.au>
2025-01-09 09:03:41 +1100

    Added debugger

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -2,10 +2,10 @@ import { nylas, nylasConfig } from "@/app/lib/nylas";
 import { redirect } from "next/navigation";
 
 export async function GET() {
-    const authURL = nylas.auth.urlForOAuth2 ({
-        clientId: nylasConfig.clientId,
-        redirectUri: nylasConfig.redirectUri,
-    })
-
-    return redirect(authURL);
+  console.log("Full redirect URI:", nylasConfig.redirectUri);
+  const authURL = nylas.auth.urlForOAuth2({
+    clientId: nylasConfig.clientId,
+    redirectUri: nylasConfig.redirectUri,
+  });
+  return redirect(authURL);
 }
